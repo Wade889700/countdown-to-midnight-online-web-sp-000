34eff21d52cc5a8718a087ad26f6b23ebb59cb57
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-17 22:00:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
-#write your code here
-
 def countdown(number)
   while number > 0
-    sleep 1
+   sleep 1
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  puts "HAPPY NEW YEAR!"
-
+  return "HAPPY NEW YEAR!"
 end
+
+countdown(10)
