ddd848cc2713db04d90363662c0a411e47104adb
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-17 21:59:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,10 @@
 
 def countdown(number)
   while number > 0
-    
+
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   puts "HAPPY NEW YEAR!"
 
-end
-
-def countdown_with_sleep
-  sleep(1)
 end
