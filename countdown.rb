91efe3bce69b70ff8915d49d8507d2858a505f47
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-17 21:59:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(number)
-  number = 12
   while number > 0
+    
     puts "#{number} SECOND(S)!"
     number -= 1
   end
@@ -11,5 +11,5 @@ def countdown(number)
 end
 
 def countdown_with_sleep
-
+  sleep(1)
 end
